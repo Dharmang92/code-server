c14174e10075b868ef156e4172bea35398e2b42d
Dharmang Gajjar <dharmang.gajjar92@gmail.com>
2023-04-01 20:29:05 +0530

    Update package.json

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const chokidar = require('chokidar');
-const watcher = chokidar.watch('./', { persistent: true });
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+const chokidar = require("chokidar");
+const watcher = chokidar.watch("./", { persistent: true });
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 
-watcher.on('change', async path => {
-    if (__filename.includes(path)) return;
-    console.clear();
-    console.log(`File Changed: ${path}`);
-    const out = await getConsoleOutput(path);
+watcher.on("change", async (path) => {
+  if (__filename.includes(path)) return;
+  console.clear();
+  console.log(`File Changed: ${path}`);
+  const out = await getConsoleOutput(path);
 
-    console.log('Output: ');
-    console.log(out.stdout);
-    console.log('Errors: ', out.stderr === '' ? 0 : out.stderr);
+  console.log("Output: ");
+  console.log(out.stdout);
+  console.log("Errors: ", out.stderr === "" ? 0 : out.stderr);
 });
 
 async function getConsoleOutput(fileName) {
-    const output = await exec(`node ${fileName}`);
-    return output;
-};
+  const output = await exec(`node ${fileName}`);
+  return output;
+}
---
+++ wordPattern.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+  return false;
+};
+
+console.log(wordPattern("abbc", "dog cat cat fish"));
